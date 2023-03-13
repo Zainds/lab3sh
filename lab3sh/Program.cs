89ee33249671449e299f49c136fd9b2aa681f96f
Ангальдт Михаил <beat1097@mail.ru>
2023-03-14 00:10:17 +0700

    Initial commit

---
+++ lab3sh/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab3sh
+{
+    internal class Program
+    {
+        public static int square(int a, int b){
+            return a*b;
+        }
+        public static void Main(string[] args)
+        {
+            
+            int x, y;
+            Console.WriteLine("Введите первую сторону пр-угольника");
+            x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону пр-угольника");
+            y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Площадь пр-угольника = " + square(x, y));
+        }
+    }
+}
---
+++ t2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace t2
+{
+    internal class Program
+    {
+        public static void fillArr(int[] arr, int n){
+            Random rnd = new Random();
+            for (int i = 0; i < n; i ++){
+                arr[i] = rnd.Next(-30, 30+1);
+            }
+        }
+        public static void printArr(int[] arr, int n){
+            for (int i = 0; i < n; i ++){
+                Console.WriteLine(i+1 + " элемент массива:  " + arr[i] );
+            }
+        }
+        public static void checkArr(int[] arr, int n){
+            int min_ind = 0;
+            double sr = 0; int k = 0;
+            for (int i = 0; i < n; i ++){
+                if (arr[i] < arr[min_ind]){
+                    min_ind = i;
+                }
+            }
+            for (int i = 0; i< min_ind; i++){
+                sr += arr[i];
+                k++;
+            }
+            Console.WriteLine("Ответ:  " + sr/k);
+        }
+        public static void Main(string[] args)
+        {
+            int n = 6;
+            int[] arr = new int[n];
+            fillArr(arr, n);
+            printArr(arr, n);
+            checkArr(arr, n);
+        }
+    }
+}
